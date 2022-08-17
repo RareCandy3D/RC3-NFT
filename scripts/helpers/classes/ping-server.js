4283b6e51988c1117b9961aa05116f6af2ffd5b1
Hendobox <50964581+Hendobox@users.noreply.github.com>
2022-08-17 22:58:15 +0100

    ipfs schema

--- scripts/helpers/classes/ping-server.js
+++ scripts/helpers/classes/ping-server.js
@@ -1,26 +1,23 @@
-const axios=require("axios");
+const axios = require("axios");
 const log = require("../../../config/log4js");
 
-
-class PingServer{
-
-    async ping(){
-        if(process.env.PING_SERVER){
-            setInterval(function(){
-             axios.get(process.env.PING_SERVER).then(
-               (response) => {
-                 log.info(`✅ Ping successful`);
-                 console.log(`✅ Ping successful`);
-               },
-               (error) => {
-                 log.info(`🚩 Ping failed ${error}`);
-               console.log(`🚩 Ping failed ${error}`);
-               }
-             );
-           }, 3480000);
-           }
+class PingServer {
+  async ping() {
+    if (process.env.PING_SERVER) {
+      setInterval(function () {
+        axios.get(process.env.PING_SERVER).then(
+          (response) => {
+            log.info(`✅ Ping successful`);
+            console.log(`✅ Ping successful`);
+          },
+          (error) => {
+            log.info(`🚩 Ping failed ${error}`);
+            console.log(`🚩 Ping failed ${error}`);
+          }
+        );
+      }, 3480000);
     }
+  }
 }
 
-
-module.exports=PingServer;
+module.exports = PingServer;
